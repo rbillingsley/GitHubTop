db423edc44e356c1caeaa1f39b54f2b866cfd7b5
Russell Billingsley <rbillingsley@users.noreply.github.com>
2017-06-06 14:12:44 +0100

    Initial commit of base app with dependencies

---
+++ git_hub_top/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import config from './config.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>The Top Github Repos </h2>
+        </div>
+        <p className="App-intro">
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;

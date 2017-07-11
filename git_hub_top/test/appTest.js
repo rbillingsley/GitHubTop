547e3878009f20aff796dff41b99c19840e44179
Russell Billingsley <rbillingsley@users.noreply.github.com>
2017-07-11 17:51:35 +0100

    setting up tests

---
+++ git_hub_top/test/appTest.js
@@ -0,0 +1,12 @@
+import { expect } from 'chai';
+import React from 'react';
+import {
+	renderIntoDocument,
+	findRenderedDOMComponentWithTag,
+	Simulate
+} from 'react-addons-test-utils';
+
+
+import app from './../src/app';
+
+Describe()

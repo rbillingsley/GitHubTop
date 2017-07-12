8d7d650c16089cc49d406490dfca9bb0109ed450
Russell Billingsley <rbillingsley@users.noreply.github.com>
2017-07-12 01:12:48 +0100

    Fixed makeRequest() tests

--- git_hub_top/test/app.test.js
+++ git_hub_top/test/app.test.js
@@ -10,16 +10,20 @@ import RepoList from '../src/repoList';
 chai.use(chaiEnzyme);
 
 describe('Component: App', () => {
-  it('calls componentDidMount()', () => {
-    sinon.spy(App.prototype, 'componentDidMount');
-    const wrapper = mount(<App />);
-    expect(App.prototype.componentDidMount.calledOnce).to.equal(true);
-  });
+  describe('Post-mount', () => {
+    it('calls componentDidMount()', () => {
+      sinon.spy(App.prototype, 'componentDidMount');
+      const wrapper = mount(<App />);
+      expect(App.prototype.componentDidMount.calledOnce).to.equal(true);
+      App.prototype.componentDidMount.restore();
+    });
 
-  it('calls makeRequest() from componentDidMount()', () => {
-    sinon.spy(App.makeRequest, 'makeRequest');
-    const wrapper = mount(<App/>);
-    expect(App.makeRequest.calledOnce).to.equal(true);
+    it('calls makeRequest() from componentDidMount()', () => {
+      sinon.spy(App.prototype, 'makeRequest');
+      const wrapper = mount(<App />);
+      expect(App.prototype.makeRequest.calledOnce).to.equal(true);
+      App.prototype.makeRequest.restore();
+    });
   });
 
   describe('App-query-button', () => {
@@ -27,13 +31,20 @@ describe('Component: App', () => {
       const wrapper = shallow(<App />);
       expect(wrapper.find('App-query-button'));
     });
-    it('should call makeRequest() when clicked');
+    
+    it('should call makeRequest() when clicked', () => {
+      sinon.spy(App.prototype, 'makeRequest');
+      const wrapper = shallow(<App />);
+      wrapper.find('button').simulate('click');
+      expect(App.prototype.makeRequest.calledOnce).to.equal(true);
+      App.prototype.makeRequest.restore();
+    });
   });
 
   describe('App-body', () => {
     it('should render a div with "App-body" class', () => {
       const wrapper = shallow(<App />);
-      expect(wrapper.find(<RepoList/>));
+      expect(wrapper.find(<RepoList />));
     });
   });
 });
